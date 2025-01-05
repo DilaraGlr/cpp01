3adf6c6075d67628f9b5822f42e5f2564ede941e
Dilara Guler <dilaraguler@MacBook-Air-de-Dilara.local>
2025-01-05 16:25:51 +0100

    ex02 finish

---
+++ ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout << "Memory address of the string variable: " << &str << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+    std::cout << "The value of the string variable: " << str << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+}
