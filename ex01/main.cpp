bcea1c39a1341a2da506b5e502106bb23f42f22f
Dilara Guler <dilaraguler@MacBook-Air-de-Dilara.local>
2025-01-05 14:54:57 +0100

    finish

---
+++ ex01/main.cpp
@@ -0,0 +1,29 @@
+#include "Zombie.hpp"
+
+int main()
+{
+    int N = 5; // Nombre de zombies
+    std::string name = "ZombieName";
+
+    // Création d'une horde de zombies
+    Zombie* horde = zombieHorde(N, name);
+
+    // Vérification si la horde a été créée
+    if (horde != NULL)
+    {
+        // Faire annoncer chaque zombie
+        for (int i = 0; i < N; ++i)
+        {
+            horde[i].announce();
+        }
+
+        // Libérer la mémoire de la horde
+        delete[] horde;
+    }
+    else
+    {
+        std::cerr << "Failed to create zombie horde." << std::endl;
+    }
+
+    return 0;
+}
